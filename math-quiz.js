6ea82d05bf10f9a8c823a00fb38354a3926fcd04
Md Abu Al Saeid <khanrajon5@gmail.com>
2023-09-30 14:09:43 +0300

    fuction added

--- math-quiz.js
+++ math-quiz.js
@@ -1,3 +1,8 @@
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function generateMathProblem() {
+    let num1 = generateRandomNumber(1, 10);
+    let num2 = generateRandomNumber(1, 10);
+    let operator = ['+', '-', '*'][generateRandomNumber(0, 2)];
