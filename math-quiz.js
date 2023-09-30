63a7b8331188c78c65b30c32e9d6d9bddeffb1c9
Md Abu Al Saeid <khanrajon5@gmail.com>
2023-09-30 14:10:19 +0300

    let answer added

--- math-quiz.js
+++ math-quiz.js
@@ -6,3 +6,16 @@ function generateMathProblem() {
     let num1 = generateRandomNumber(1, 10);
     let num2 = generateRandomNumber(1, 10);
     let operator = ['+', '-', '*'][generateRandomNumber(0, 2)];
+
+    let answer;
+    switch (operator) {
+        case '+':
+            answer = num1 + num2;
+            break;
+        case '-':
+            answer = num1 - num2;
+            break;
+        case '*':
+            answer = num1 * num2;
+            break;
+    }
